1d989068c15cc54519e46031b1a174b761f3212f
Akrwt-dev <volcanoak1@gmail.com>
2025-09-27 18:21:23 +0530

    setting up Express

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Home page");
+// here order is very importent 
+app.use("/test/2", (req, res) => {
+  res.send("this is the second test page");
 });
 
-app.get("/test", (req, res) => {
-  res.send("Test page");
+app.use("/test", (req, res) => {
+  res.send("First Test page");
+});
+// this will never show because of "/test" 
+app.use("/test/3", (req, res) => {
+  res.send("Third Test page");
 });
 
-app.get("/user", (req, res) => {
+app.use("/user", (req, res) => {
   res.send("User page");
+});
+
+app.use("/", (req, res) => {
+  res.send("Home page");
 });
 
 app.listen(4000, () => {
