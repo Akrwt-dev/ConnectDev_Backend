cfceca8240ec9e5c47d42db9e6969c0c65e158b3
Akrwt-dev <volcanoak1@gmail.com>
2025-09-30 20:00:28 +0530

    create api

--- src/app.js
+++ src/app.js
@@ -5,6 +5,22 @@ const User = require("./modules/user.js");
 
 app.use(express.json())
 
+app.get("/user",async(req,res)=>{
+  const userEmail = req.body.emailID
+
+  try{
+    const users = await User.find({emailID : userEmail})
+    if(users.length===0){
+      res.status(400).send("User not found" + err)
+    }else{
+       res.send(users)
+    }
+  }catch(err){
+    res.status(400).send("Something went wrong" + err)
+  }
+
+})
+
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   await user.save();
